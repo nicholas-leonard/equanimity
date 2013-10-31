343324973b13bcbc55b34eeb4628cd9108f279fd
Nicholas Leonard <nick@nikopia.org>
2013-10-31 18:38:27 -0400

    neuralnetwork++

--- neuralnetwork.lua
+++ neuralnetwork.lua
@@ -8,5 +8,124 @@ train_data = dataset.Mnist{which_set='train'}
 valid_data = dataset.Mnist{which_set='valid'}
 test_data = dataset.Mnist{which_set='test'}
 
---
+ninputs = train_data.n_dimensions()
+nhidden = 200
+noutputs = 10
+batch_size = 32
 
+--Build a neural network:
+model = nn.Sequential()
+model:add(nn.Reshape(ninputs))
+model:add(nn.Linear(ninputs,nhiddens))
+model:add(nn.Tanh())
+model:add(nn.Linear(nhiddens,noutputs))
+
+--Build a training algorithm:
+optimState = {
+      learningRate = 0.001,
+      momentum = 0.7,
+      learningRateDecay = 1e-7
+   }
+   optimMethod = optim.sgd
+   
+function train()
+
+   -- epoch tracker
+   epoch = epoch or 1
+
+   -- local vars
+   local time = sys.clock()
+
+   -- shuffle at each epoch
+   shuffle = torch.randperm(train_data:size())
+
+   -- do one epoch
+   print('==> doing epoch on training data:')
+   print("==> online epoch # " .. epoch .. ' [batchSize = ' .. opt.batchSize .. ']')
+   for t = 1,train_data:size(),batch_size do
+      -- disp progress
+      xlua.progress(t, train_data:size())
+
+      -- create mini batch
+      local inputs = {}
+      local targets = {}
+      for i = t,math.min(t+opt.batchSize-1,trainData:size()) do
+         -- load new sample
+         -- TODO : modify this to work with tensor batches:
+         local input = train_data.data[shuffle[i]]
+         local target = train_data.labels[shuffle[i]]
+         if opt.type == 'double' then input = input:double()
+         elseif opt.type == 'cuda' then input = input:cuda() end
+         table.insert(inputs, input)
+         table.insert(targets, target)
+      end
+
+      -- create closure to evaluate f(X) and df/dX
+      local feval = function(x)
+                       -- get new parameters
+                       if x ~= parameters then
+                          parameters:copy(x)
+                       end
+
+                       -- reset gradients
+                       gradParameters:zero()
+
+                       -- f is the average of all criterions
+                       local f = 0
+
+                       -- evaluate function for complete mini batch
+                       for i = 1,#inputs do
+                          -- estimate f
+                          local output = model:forward(inputs[i])
+                          local err = criterion:forward(output, targets[i])
+                          f = f + err
+
+                          -- estimate df/dW
+                          local df_do = criterion:backward(output, targets[i])
+                          model:backward(inputs[i], df_do)
+
+                          -- update confusion
+                          confusion:add(output, targets[i])
+                       end
+
+                       -- normalize gradients and f(X)
+                       gradParameters:div(#inputs)
+                       f = f/#inputs
+
+                       -- return f and df/dX
+                       return f,gradParameters
+                    end
+
+      -- optimize on current mini-batch
+      if optimMethod == optim.asgd then
+         _,_,average = optimMethod(feval, parameters, optimState)
+      else
+         optimMethod(feval, parameters, optimState)
+      end
+   end
+
+   -- time taken
+   time = sys.clock() - time
+   time = time / trainData:size()
+   print("\n==> time to learn 1 sample = " .. (time*1000) .. 'ms')
+
+   -- print confusion matrix
+   print(confusion)
+
+   -- update logger/plot
+   trainLogger:add{['% mean class accuracy (train set)'] = confusion.totalValid * 100}
+   if opt.plot then
+      trainLogger:style{['% mean class accuracy (train set)'] = '-'}
+      trainLogger:plot()
+   end
+
+   -- save/log current net
+   local filename = paths.concat(opt.save, 'model.net')
+   os.execute('mkdir -p ' .. sys.dirname(filename))
+   print('==> saving model to '..filename)
+   torch.save(filename, model)
+
+   -- next epoch
+   confusion:zero()
+   epoch = epoch + 1
+end

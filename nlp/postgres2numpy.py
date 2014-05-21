067cd4e5ade906d0c579dbd7b028747e85dc8c3a
nicholas-leonard <nick@nikopia.org>
2014-05-21 14:53:18 -0400

    added script for mehdi

---
+++ nlp/postgres2numpy.py
@@ -0,0 +1,82 @@
+------------------------------------------------------------------------
+-- Generate a serialized billion-words dataset from its 
+-- PostgreSQL representation. Each word is an int. 
+------------------------------------------------------------------------
+
+from database import DatabaseHandler #python
+
+cmd = torch.CmdLine()
+cmd:text()
+cmd:text('Generate torch dump for billion-words dataset')
+cmd:text('Example:')
+cmd:text('$> th postgres2torch.lua --dataset train')
+cmd:text('$> th postgres2torch.lua --treeTable "bw.word_cluster" --treeFile "word_tree1.th7"')
+cmd:text('Options:')
+cmd:option('--dataset', 'train', 'train | valid | test | small | tiny')
+cmd:option('--stepSize', 1000, 'amount of sentences to retrieve per query')
+
+cmd:option('--wordMap', false, 'outputs a mapping of word strings to word integers')
+
+cmd:option('--treeTable', '', 'Name of the table containing a hierarchy of words. Used for hierarchical softmaxes.')
+cmd:option('--treeFile', '', 'Name of the file where to save the torch dump.')
+
+opt = cmd:parse(arg or {})
+
+local pg = DatabaseHandler() #python
+
+# where to save data:
+local data_path = paths.concat(dp.DATA_DIR, 'BillionWords')
+dp.check_and_mkdir(data_path)
+
+# python
+n_word = pg.executeSQL('''
+      SELECT SUM(array_upper(sentence_words, 1)-1) FROM bw.%s_sentence
+      ''', (self.sbjCluster.key, self.sbjCluster.key), pg.FETCH_ONE
+   )[0]
+
+local data = torch.IntTensor(n_word, 2)
+--sequence of words where sentences are delimited by </s>
+local corpus = data:select(2, 2)
+--holds start indices of sentence of word at same index in corpus
+local delimiters = data:select(2, 1)
+
+
+local n_sentence = tonumber(
+   pg:fetchOne(
+      "SELECT MAX(sentence_id) FROM bw.%s_sentence ", {opt.dataset}
+   )[1]
+)
+local step = opt.stepSize
+local start_time = os.time()
+print("loading " .. n_word .. " words or " .. n_sentence .. " sentences from db")
+
+local sentence_idx = 1
+for i=1,n_sentence,step do
+   local rows = pg:fetch(
+      "SELECT sentence_id, sentence_words " ..
+      "FROM bw.%s_sentence " ..
+      "WHERE sentence_id BETWEEN %d and %d", 
+      {opt.dataset, i, math.min(i+step-1, n_sentence)}
+   )
+   for j, row in ipairs(rows) do
+      local sentence_id = tonumber(row[1])
+      local sentence_words = torch.IntTensor(
+         _.map(
+            _.split(string.sub(row[2], 9, -2), ','), 
+            function(k, str) return tonumber(str) end
+         )
+      )
+      local sentence_size = sentence_words:size(1)
+      corpus:narrow(1, sentence_idx, sentence_size):copy(sentence_words)
+      delimiters:narrow(1, sentence_idx, sentence_size):fill(sentence_idx)
+      sentence_idx = sentence_idx + sentence_size
+   end
+   xlua.progress(i, n_sentence)
+end
+print("\nloaded " .. n_sentence .. " sentences in " .. os.time()-start_time .. " sec.")
+
+--print(ngrams[{{ngrams:size(1)-100, ngrams:size(1)},{}}])
+--print(corpus[{{corpus:size(1)-100, corpus:size(1)}}])
+
+
+torch.save(paths.concat(data_path, opt.dataset..'_data.th7'), data)
